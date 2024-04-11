f978eb863e8a4b39198ef6d274e4f2ac9b662d1d
Alisson Zampietro <alisson.zampietro@bluepanda.pt>
2024-04-11 22:21:47 +0100

    calling external api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import logo from './logo.svg';
 import { Header } from './components/Header';
 import { MoviesList } from './components/MoviesList';
 import { MoviesContainer } from './styles/MoviesContainer';
+import { useEffect, useState } from 'react';
+import MoviesService from './api/MoviesService';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const { data } = await MoviesService.getMovies();
+    setMovies(data.results);
+  }
+
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <MoviesContainer>
-        <MoviesList movies={[]}/>
+        <MoviesList movies={movies}/>
       </MoviesContainer>
     </div>
   );
---
+++ src/api/MoviesService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_KEY = '47211058bd7df482efbdf0320c135d57';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export default class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'))
+    }
+}
+
+const path = 'https://api.themoviedb.org/3/'
